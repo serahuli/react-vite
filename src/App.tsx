5886fb8257a76414aa381fa01dd3db5097518a4f
sera.hu <hulixiasera@gmail.com>
2021-11-03 09:51:51 +0800

    ci(eslint): 配置 eslint 环境

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-
-
-  const a: string = '1'
-  return (
-    <div className="App">
-      {a}
-    </div>
-  );
+    const a: string = '1';
+    return <div className="App">{a}</div>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, 'src'),
-      '@components': resolve(__dirname, 'src/components'),
-      '@container': resolve(__dirname, 'src/container'),
-      '@hooks': resolve(__dirname, 'src/hooks'),
-      '@images': resolve(__dirname, 'src/images'),
-      '@style': resolve(__dirname, 'src/style'),
-      '@utils': resolve(__dirname, 'src/utils')
-    },
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
-  }
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@components': resolve(__dirname, 'src/components'),
+            '@container': resolve(__dirname, 'src/container'),
+            '@hooks': resolve(__dirname, 'src/hooks'),
+            '@images': resolve(__dirname, 'src/images'),
+            '@style': resolve(__dirname, 'src/style'),
+            '@utils': resolve(__dirname, 'src/utils')
+        },
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
+    }
 });
